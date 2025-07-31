7cb12cd94b17a82287fef1d64266741202231e7e
MaxKarpach <127778887+MaxKarpach@users.noreply.github.com>
2025-07-31 21:09:12 +0300

    Страницы, роутер и компоненты

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
 </script>
 
 <template>
-    <RouterView></RouterView>
+    <div>
+        Main
+    </div>
 </template>
 
 <style scoped lang="scss">
